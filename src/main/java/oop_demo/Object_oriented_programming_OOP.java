042dae12552aa6f0d71964e81dd6fa84218415be
offic <offic@DESKTOP-GLF4UNU>
2024-07-03 12:59:08 +0600

    Introduction to OOP.

---
+++ src/main/java/oop_demo/Object_oriented_programming_OOP.java
@@ -0,0 +1,8 @@
+package oop_demo;
+
+public class Object_oriented_programming_OOP {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"object oriented programming_OOP.\"");
+    }
+}
