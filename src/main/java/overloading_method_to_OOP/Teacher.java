1bf645c1b7e863a0daaf5c53ee40a41a695e820e
offic <offic@DESKTOP-GLF4UNU>
2024-07-03 14:54:57 +0600

    Difference between Constructor and Method.

--- src/main/java/overloading_method_to_OOP/Teacher.java
+++ src/main/java/overloading_method_to_OOP/Teacher.java
@@ -2,17 +2,62 @@ package overloading_method_to_OOP;
 
 public class Teacher {
 
-    //এখানে ২টি class use করা হয়েছে।। এই class-এ method declare করে, অন্য class থেকে তাকে call করা হয়েছে।।
-    //return value from method------------
-    double square(double value) {
-        return value * value;
+    //instance variable
+    String name, gender;
+    int id;
+
+    //default constructor
+    Teacher() {
+        //System.out.println("no value");
     }
+
+    //parametrized constructor
+    Teacher(String n, String g) {
+        name = n;
+        gender = g;
+    }
+
+    //parametrized constructor
+    Teacher(String n, String g, int id) {
+        name = n;
+        gender = g;
+        this.id = id;
+    }
+    //All of the above are constructor
+
+    //it is method
+    void displayInformation() {
+        System.out.println("Name   : " + name);
+        System.out.println("gender : " + gender);
+        System.out.println("ID     : " + id);
+        System.out.printf("\n");
+    }
+
+    //constructor method diclare -----------
     /*
-    int square (){
-        return 5*5;
-    }
-    এভাবে দিলে হবে না,
-    অর্থাৎ কোন parametre না দিয়ে সরাসরি কোন number declare করলে, user যেকোন number type করলেও শুধু এ number-এর result প্রদান করবে।।
-    ()first bracket এর ভিতরে যা থাকে, তারই নাম parametre.
+    Constructor is a special type of method that is used to initialize the object.
+    Properties of constructor:
+        contructor has the same name as that of the class it belongs.
+        constructor is a special type of method.
+        it has no return type not even void.
+        it is called automatically.
+        Defult constructor (no parameter), parametrized constructor.
+        You can take many Contructor, but their parameter must be different.
+        class name & constructor name must be same.
+     */
+
+    /*
+    Difference between Constructor and Method
+    
+    Constructor:
+        1- its name should be same as the class name.
+        2- its does not have any return type.
+        3- if there is no constructor then the default constructor is created by the compiler itself.
+    
+    Method:
+        1- its name should not same as the class name. 
+        2- it may return a value.
+        3- in case of method, there is no default method provided.
+    
      */
 }
--- src/main/java/overloading_method_to_OOP/Test.java
+++ src/main/java/overloading_method_to_OOP/Test.java
@@ -5,19 +5,14 @@ public class Test {
     public static void main(String[] args) {
 
         //এখানে ২টি class use করা হয়েছে।। এই class-এ object create করে, অন্য class কে call করা হয়েছে।।
-        //How to return a value from method ?
-        //one system -------------------------
-        Teacher teacher1 = new Teacher();
-        double result = teacher1.square(5);
-        System.out.println("square of 5 =  " + result);
+        Teacher teacher = new Teacher();
+        teacher.displayInformation();
 
-        Teacher teacher2 = new Teacher();
-        double result2 = teacher2.square(9);
-        System.out.println("square of 9 =  " + result2);
+        Teacher teacher1 = new Teacher("kamrul islam", "male");
+        teacher1.displayInformation();
 
-        //two system. ------------------------
-        Teacher teacher3 = new Teacher();
-        System.out.println("square of 6 =  " + teacher3.square(6));
+        Teacher teacher2 = new Teacher("kamrul", "male", 123456);
+        teacher2.displayInformation();
 
     }
 }
