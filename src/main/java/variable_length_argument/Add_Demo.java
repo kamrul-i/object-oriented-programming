938a72a87e93793ef2985c6099556f7d0d8a8124
offic <offic@DESKTOP-GLF4UNU>
2024-07-09 13:19:24 +0600

    Variable length argument updated.

--- src/main/java/variable_length_argument/Add_Demo.java
+++ src/main/java/variable_length_argument/Add_Demo.java
@@ -3,24 +3,23 @@ package variable_length_argument;
 public class Add_Demo {
     //এখানে ২টি class use করা হয়েছে।। main class থেকে এই class কে call করা হয়েছে।।
 
-    void add(int num1, int num2) {
-        System.out.println(num1 + num2);
-    }
+    void add(int... num) {
 
-    void add(int num1, int num2, int num3) {
-        System.out.println(num1 + num2 + num3);
-    }
-
-    void add(int num1, int num2, int num3, int num4) {
-        System.out.println(num1 + num2 + num3 + num4);
+        int sum = 0;
+        for (int x : num) {
+            sum = sum + x;
+        }
+        System.out.print(sum + " ");
+        //System.out.println(sum);
     }
 
     /*
     variable length argument -কে varargs method বলে।।
     A method that takes a variable number of arguments is called a varargs method.
     
-    এখানে ২টি, ৩টি, ৪টি parametre declare করা হয়েছে, এবং main method এর মাধ্যমে result show করা হয়েছে,
-    এভাবে যতটি parametre যুক্ত method declare করবেন, main method-এ ততটি arguments declare করতে হবে, এভাবে আর কত করবেন!!
-    পরবর্তী commit-এ update দেখুন।।
+    main method-এ যতই arguments declare করুন, 
+    class-এ method এর মধ্যে 3 dot(...) দিলে সকল arguments কে control করা সম্ভব।। 
+    এখানে for each loop ব্যবহার করা হয়েছে।।
+
      */
 }
--- src/main/java/variable_length_argument/Test.java
+++ src/main/java/variable_length_argument/Test.java
@@ -7,13 +7,8 @@ public class Test {
 
         Add_Demo ad = new Add_Demo();
         ad.add(10, 20);
-        ad.add(10, 20, 30);
-        ad.add(54, 90, 82, 32);
+        ad.add(30, 20, 40);
+        ad.add(10, 50, 30, 70);
+        ad.add(10, 80, 30, 90, 50);
     }
-    /*
-    এখানে ২টি, ৩টি, ৪টি arguments declare করা হয়েছে, এবং class কে call করা হয়েছে, 
-    এভাবে যতটি arguments যুক্ত object declare করবেন, class-এ ততটি method declare করতে হবে, এভাবে আর কত করবেন!!
-    পরবর্তী commit-এ update দেখুন।।
-     */
-
 }
