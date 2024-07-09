10302918fed66da0075bc34b8f75bed1e2f8b088
offic <offic@DESKTOP-GLF4UNU>
2024-07-09 15:11:07 +0600

    Difference between iteration & recursion.

---
+++ src/main/java/iteration_vs_recursion/Iteration_vs_Recursion.java
@@ -0,0 +1,22 @@
+package iteration_vs_recursion;
+
+public class Iteration_vs_Recursion {
+
+    /*
+    Difference between iteration & recursion.
+    
+    Iteration:
+    Iteration এর ক্ষেত্রে (for, while, do-while loop) ব্যবহার করা হয়,
+    Terminates when continuation condition fails.
+    Iteration is fast.
+    Iteration code is bigger.
+    
+    Recursion:
+    Recursion এর ক্ষেত্রে (if-else,switch) ব্যবহার করা হয়,
+    Terminates when base case is satisfied.
+    recursion is slow.
+    recursion code is smaller.
+    
+
+     */
+}
