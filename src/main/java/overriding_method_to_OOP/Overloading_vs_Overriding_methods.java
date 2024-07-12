75ea083cbdab2da239f13071344854144a876b04
offic <offic@DESKTOP-GLF4UNU>
2024-07-12 15:12:23 +0600

    Difference between Overloading & Overriding methods.

---
+++ src/main/java/overriding_method_to_OOP/Overloading_vs_Overriding_methods.java
@@ -0,0 +1,26 @@
+package overriding_method_to_OOP;
+
+public class Overloading_vs_Overriding_methods {
+
+    /*
+    Difference between Overloading & Overriding method:
+    
+    method overriding:
+        1. Super class এর method-কে same নামে sub-class এর মধ্যে declare করাকে method overriding বলে।।
+        2. final & static methods-কে override করা যায় না।।
+        3. java main method-কে override করা যায় না।।
+
+        4. যে method-কে override করা হবে, তার নাম উভয় class-এ ‍same হতে হবে, তবে তাদের parametre list ভিন্ন হতে হবে।।
+        5. এই method-কে overriden করে নিতে হয়, 
+        6. তবে তার আগে super class-কে extends করে নিতে হবে।। extends-কে inherit বলে।।
+        7. return type একই হতে পারে, ভিন্ন হতে পারে।। যেমন, (void)
+        8. method override কে run time polymorphism বলে।।
+    
+    method Overloading:
+        1. একই class এর মধ্যে same নামে কয়েকটি method declare করাকে method Overloading বলে।।
+        2. সকল methods একই নামে হতে হবে এবং একটি class এর মধ্যে থাকতে হবে, তবে তাদের parametre list ভিন্ন হতে হবে।।
+        3. return type একই হতে হবে।।
+        4. method overloading, constructor overloading কে static polymorphism বলে।।
+
+     */
+}
